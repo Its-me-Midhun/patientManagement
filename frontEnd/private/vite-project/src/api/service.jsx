e43c58fe47097e47a2916c2abec86be8da5e0b0d
Its-me-Midhun <125955324+Its-me-Midhun@users.noreply.github.com>
2023-05-24 23:43:05 +0530

    Delete frontEnd directory

--- frontEnd/private/vite-project/src/api/service.jsx
+++
@@ -1,9 +0,0 @@
-import postgresInstance from './instance';
-
-export const getData = (url) => postgresInstance.get(url);
-
-export const setData = (url, data) => postgresInstance.post(url, data);
-
-export const editData = (url, data) => postgresInstance.patch(url, data);
-
-export const deleteData = (url) => postgresInstance.delete(url);
